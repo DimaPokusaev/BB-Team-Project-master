9d16992d63b974acde7aa4f90d8762897107a228
Dima Pokusaev <dmitpoku109@ed.amdsb.ca>
2019-04-25 09:40:33 -0400

    Add project files.

---
+++ BrickBreaker/Ball.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BrickBreaker
+{
+    public class Ball
+    {
+        public int xSpeed, ySpeed, size;
+        public double Xangle, Yangle, x, y;
+        public Color colour;
+
+        public static Random rand = new Random();
+        
+        public Ball(int _x, int _y, int _xSpeed, int _ySpeed, int _ballSize, double _Xangle, double _Yangle)
+        {
+            x = _x;
+            y = _y;
+            xSpeed = _xSpeed;
+            ySpeed = _ySpeed;
+            size = _ballSize;
+            Xangle = _Xangle;
+            Yangle = _Yangle;      
+        }
+
+        public void Move()
+        {
+            x = x + xSpeed*Xangle;
+            y = y + ySpeed*Yangle;
+        }
+
+        public bool BlockCollision(Block b)
+        {
+            Rectangle blockRec = new Rectangle(Convert.ToInt32(b.x), Convert.ToInt32(b.y), b.width, b.height);
+            Rectangle ballRec = new Rectangle(Convert.ToInt32(x), Convert.ToInt32(y), size, size);
+
+            if (ballRec.IntersectsWith(blockRec))
+            {
+                ySpeed *= -1;
+            }
+
+            return blockRec.IntersectsWith(ballRec);         
+        }
+
+        public void PaddleCollision(Paddle p, bool pMovingLeft, bool pMovingRight)
+        {
+            Rectangle ballRec = new Rectangle(Convert.ToInt32(x), Convert.ToInt32(y), size, size);
+            Rectangle paddleRec = new Rectangle(p.x, p.y - 2, p.width, p.height);
+
+            if (ballRec.IntersectsWith(paddleRec))
+            {
+                ySpeed *= -1;
+
+                if (pMovingLeft)
+                    xSpeed = -Math.Abs(xSpeed);
+                else if (pMovingRight)
+                    xSpeed = Math.Abs(xSpeed);
+            }
+        }
+
+        public void WallCollision(UserControl UC)
+        {
+            // Collision with left wall
+            if (x <= 0)
+            {
+                x = 1;
+                xSpeed *= -1;
+            }
+            // Collision with right wall
+            if (x >= (UC.Width - size))
+            {
+                x = UC.Width - size - 1;
+                xSpeed *= -1;
+            }
+            // Collision with top wall
+            if (y <= 2)
+            {
+                y = 0;
+                ySpeed *= -1;
+            }
+        }
+
+        public bool BottomCollision(UserControl UC, Paddle p)
+        {
+            if (y + size -4 > p.y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ BrickBreaker/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BrickBreaker
+{
+    public class Block
+    {
+        public int width = 50;
+        public int height = 25;
+
+        public int x;
+        public int y; 
+        public int hp;
+        public int score; // TODO: Have bricks in a designed level have more points or less
+        
+        /// <summary>
+        /// Gets the color based on the hp
+        /// </summary>
+        /// <returns></returns>
+        public Color colour()
+        {
+            switch(hp)
+            {
+                case 1:
+                    return Color.Red;
+                case 2:
+                    return Color.Yellow;
+                case 3:
+                    return Color.Green;
+                default:
+                    return Color.Orange;
+            }
+        }
+
+        public static Random rand = new Random();
+
+        public Block(string _x, string _y, string _hp)
+        {
+            x = Convert.ToInt32(_x);
+            y = Convert.ToInt32(_y);
+            hp = Convert.ToInt32(_hp);
+        }
+    }
+}
